6cb5f3cf1868adc22a9914c0d705d9fbae4f6a76
Brateq <brateq@gmail.com>
2014-02-04 12:02:13 +0000

    rails new bartcrm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bartcrm::Application.config.secret_key_base = '6e5c9aecdb124bc831e6ac1398514c4766f7c59ac09ba0ed91df3980957d568d347f2f26c5b98a91e001f539cbc91002e204440abf8a9411441c6a08cdc50d60'
