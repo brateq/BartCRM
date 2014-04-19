d6d4d160b0ccda3ac953accf71190c7d0ac8160b
Brateq <brateq@gmail.com>
2014-04-19 15:50:50 +0000

    Add gem chartkick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+<%= javascript_include_tag "//www.google.com/jsapi", "chartkick" %>
 //= require jquery
 //= require jquery_ujs
 //= require select2
