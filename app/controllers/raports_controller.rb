002ab7b0d5fbe71c816926e3ff8a3d6200d3e597
brateq <brateq@gmail.com>
2014-05-10 18:43:35 +0000

    root change to login page

--- app/controllers/raports_controller.rb
+++ app/controllers/raports_controller.rb
@@ -7,6 +7,10 @@ class RaportsController < ApplicationController
     @search = Call.all.search(params[:q])
     @calls = @search.result.page params[:page]
     @chart = Call.group_by_day(:created_at).count
+    respond_to do |format|
+      format.html
+      format.json {render Call.group_by_day(:created_at).count}
+    end
   end
 end
  
