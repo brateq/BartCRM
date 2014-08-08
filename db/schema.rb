90c6c93d46448c9de03a8ff54317e4fb87b4408d
brateq <brateq@gmail.com>
2014-08-08 12:46:45 +0000

    add fax and increase max integer to krs/nip/regon/phones

--- db/schema.rb
+++ db/schema.rb
@@ -59,15 +59,16 @@ ActiveRecord::Schema.define(version: 20140716121317) do
     t.string   "www"
     t.string   "email"
     t.string   "legal_form"
-    t.integer  "phone"
+    t.integer  "phone",              limit: 8
+    t.integer  "fax",                limit: 8
     t.string   "street"
     t.string   "postcode"
     t.string   "city"
     t.string   "country"
-    t.integer  "krs"
+    t.integer  "krs",                limit: 8
     t.text     "decription"
-    t.integer  "nip"
-    t.integer  "regon"
+    t.integer  "nip",                limit: 8
+    t.integer  "regon",              limit: 8
     t.string   "progress"
     t.string   "type_of_training"
     t.string   "trade"
