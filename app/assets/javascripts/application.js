05f0e1cba30dfde9a410c8ebc5be7e3065ecf1c7
Brateq <brateq@gmail.com>
2014-11-25 14:01:22 +0000

    add non working javascript validation to login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,74 @@ $(document).ready(function(){
   });
 });
 
+/* form validation */
+
+function isNotEmpty(field){
+	
+	if(field.value === ""){
+		errorInfo(field, "empty");
+		return false;
+	}else{
+		errorInfo(field, "ok");
+		return true;
+	}
+}
+
+function isANumber(field){
+	if(isNotEmpty(field) === true){
+		if(isNaN(field.value)){
+			errorInfo(field, "NaN");
+		}else{
+			errorInfo(field, "ok");
+		}
+	}
+}
+
+function isEmail(field){
+	if(isNotEmpty(field) === true){
+		var atPosition = field.value.indexOf("@");
+		var dotPosition = field.value.lastIndexOf(".");
+		
+		if (atPosition<1 || dotPosition+1 == field.value.length || atPosition+3 > dotPosition){
+			errorInfo(field, "wrongEmail");
+		}else{
+			errorInfo(field, "ok");
+		}
+	}
+}
+
+function errorInfo(field, status){
+	switch(status){
+		case "ok":
+			document.getElementById("i" + field.name).innerHTML = "";
+			break;
+		case "empty":
+			document.getElementById("i" + field.name).innerHTML = "Don't let me be empty!";
+			break;
+		case "NaN":
+			document.getElementById("i" + field.name).innerHTML = "This is not a number!";
+			break;
+		case "wrongEmail":
+			document.getElementById("i" + field.name).innerHTML = "This is not a valid email";
+			break;
+		}
+}
+window.onload = Init;
+
+function Init(){
+	var person = document.getElementById("person");
+	person.onblur = function(){
+		isNotEmpty(this);
+		};
+	
+	var account = document.getElementById("account");
+	account.onblur = function(){
+		isANumber(this);
+		};
+		
+	var email = document.getElementById("email");
+	email.onblur = function(){
+		isEmail(this);
+	};
+}
+
